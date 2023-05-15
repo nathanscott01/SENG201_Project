7258b7062e4d79bc017e1d6294e60c4efe3fd9cc
DominicDumble <ddu40@uclive.ac.nz>
2023-05-15 16:15:49 +1200

    Athlete class given stats for players

--- Lacrosse/src/Athlete.java
+++ Lacrosse/src/Athlete.java
@@ -1,4 +1,19 @@
+import java.math.BigDecimal;
 
-public class Athlete {
-
+public class Athlete extends Purchasable {
+	private int atkStat;
+	private int defStat;
+	private int stamina;
+	private String name;
+	private String nickName;
+	
+	public Athlete(BigDecimal cost,int aStat, int dStat, int stam, String ogName) {
+		super(cost);
+		
+		atkStat = aStat;
+		defStat = dStat;
+		stamina = stam;
+		name = ogName;
+		nickName = ogName;
+    }
 }
