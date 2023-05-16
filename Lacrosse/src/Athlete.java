14a55f396b16d18ec0254df132d12d693f0c0c37
DominicDumble <ddu40@uclive.ac.nz>
2023-05-16 16:45:31 +1200

    added getters and setters plus dropStamina for later use

--- Lacrosse/src/Athlete.java
+++ Lacrosse/src/Athlete.java
@@ -1,13 +1,12 @@
-import java.math.BigDecimal;
-
 public class Athlete extends Purchasable {
 	private int atkStat;
 	private int defStat;
 	private int stamina;
 	private String name;
 	private String nickname;
+	private int position;
 	
-	public Athlete(BigDecimal cost,int aStat, int dStat, int stam, String ogName) {
+	public Athlete(float cost,int aStat, int dStat, int stam, String ogName) {
 		super(cost);
 		
 		atkStat = aStat;
@@ -16,6 +15,15 @@ public class Athlete extends Purchasable {
 		name = ogName;
 		nickname = ogName;
     }
+	
+														/*GETTERS AND SETTERS BELOW*/
+	public void setPosition(int playerInput) {
+		position = playerInput;
+	}
+	
+	public void setNickname(String word) {
+		nickname = word;
+	}
 	
 	public int[] getStats() {
 		int[] retArray = new int[3];
@@ -32,5 +40,15 @@ public class Athlete extends Purchasable {
 		retArray[1] = nickname;
 		
 		return retArray;
+	}
+	
+	public int getPosition() {
+		return position;
+	}
+														/*GETTERS AND SETTERS ABOVE*/
+	
+	public Boolean dropStamina(int decrease) {          /*Decreases the Athletes stamina and returns True if stamina drops to 0*/
+		stamina -=decrease;
+		return stamina<=0;
 	}
 }
