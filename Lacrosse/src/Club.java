643e1239dec3435b508e307b47b7d58d1a4b3aba
DominicDumble <ddu40@uclive.ac.nz>
2023-05-16 16:46:45 +1200

    getters and setters as well as adding athlete and getting positionNum

--- Lacrosse/src/Club.java
+++ Lacrosse/src/Club.java
@@ -1,7 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Club extends GameEnvironment {
 	private String clubName;
+	private List<Athlete> team;
+	private List<Athlete> reserve;
 	
+	public Club() {                        /*Club constructor*/
+		team = new ArrayList<Athlete>();
+		reserve = new ArrayList<Athlete>();
+	}
+	
+											/*Getters and setters*/
 	public String getName() {
 		return clubName;
 	}
@@ -10,4 +20,25 @@ public class Club extends GameEnvironment {
 		clubName = newName;
 	}
 	
+	public List<Athlete> getTeam(){
+		return team;
+	}
+	
+	public List<Athlete> getReserve(){
+		return reserve;
+	}
+	
+	public int getNumPosition(int position) {
+		int retNum=0;
+		for (Athlete athlete : team) {
+			if (athlete.getPosition()==position) {
+				retNum+=1;
+			}
+		}
+		return retNum;
+	}
+	
+	public void addAthlete(Athlete athlete) {
+		team.add(athlete);
+	}
 }
