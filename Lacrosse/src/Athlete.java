7752b53d3ec22276e10f07dc6622ad024084cecf
DominicDumble <ddu40@uclive.ac.nz>
2023-05-16 14:43:38 +1200

    get functions for athlete implemented

--- Lacrosse/src/Athlete.java
+++ Lacrosse/src/Athlete.java
@@ -5,7 +5,7 @@ public class Athlete extends Purchasable {
 	private int defStat;
 	private int stamina;
 	private String name;
-	private String nickName;
+	private String nickname;
 	
 	public Athlete(BigDecimal cost,int aStat, int dStat, int stam, String ogName) {
 		super(cost);
@@ -14,6 +14,23 @@ public class Athlete extends Purchasable {
 		defStat = dStat;
 		stamina = stam;
 		name = ogName;
-		nickName = ogName;
+		nickname = ogName;
     }
+	
+	public int[] getStats() {
+		int[] retArray = new int[3];
+		retArray[0] = atkStat;
+		retArray[1] = defStat;
+		retArray[2] = stamina;
+		
+		return retArray;
+	}
+	
+	public String[] getName() {
+		String[] retArray = new String[2];
+		retArray[0] = name;
+		retArray[1] = nickname;
+		
+		return retArray;
+	}
 }
