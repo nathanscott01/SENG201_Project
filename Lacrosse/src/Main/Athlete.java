dfe177aca9a073fc49ebc6f3cbf761a0ced21b10
DominicDumble <ddu40@uclive.ac.nz>
2023-05-20 12:59:24 +1200

    added position swap

--- Lacrosse/src/Main/Athlete.java
+++ Lacrosse/src/Main/Athlete.java
@@ -65,6 +65,14 @@ public class Athlete extends Purchasable {
 		stamina -=decrease;
 		return stamina<=0;
 	}
+	
+	public void positionSwap(Athlete athlete1, Athlete athlete2) {
+		int pos_tracker;
+		pos_tracker = athlete1.getPosition();
+		athlete1.setPosition(athlete2.getPosition());
+		athlete2.setPosition(pos_tracker);
+		
+	}
 
 	@Override
 	  public String toString() {
