fa53035239a4bf6b728b9ec8c34052abe05ba3bd
Nathan Scott <scotty05nj@gmail.com>
2023-05-21 22:35:48 +1200

    Added partial javadoc for Club Ammended checkTeamFull to add Boolean teamFull = false

--- Lacrosse/src/Main/Club.java
+++ Lacrosse/src/Main/Club.java
@@ -2,35 +2,72 @@ package Main;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 
+ * @author Dominic Dumble, Nathan Scott
+ * 
+ * Club Class extends the methods in GameEnviroment and controls the team and reserve lists.
+ * The Club Class implements methods which swap athletes on and off the team, and in and out of
+ * the reserves, as well as displaying who can and can't play in matches
+ *
+ */
+
 public class Club extends GameEnvironment {
-	private String clubName;
-	private List<Athlete> team;
-	private List<Athlete> reserve;
+	private String clubName;				// Name of the club
+	private List<Athlete> team;				// List of athletes playing in the team
+	private List<Athlete> reserve;			// List of athletes in the reserves
 	
-	public Club() {                        /*Club constructor*/
+	/**
+	 * This function initialises the Club by setting up the team and reserve lists
+	 */
+	public Club() {
 		team = new ArrayList<Athlete>();
 		reserve = new ArrayList<Athlete>();
 	}
 	
-											/*Getters and setters*/
+	/*Getters and setters*/
+	
+	/**
+	 * This function retrieves the name of the Club
+	 * @return clubName
+	 */
 	public String getName() {
 		return clubName;
 	}
 	
+	/**
+	 * Sets the name of the club
+	 * @param newName is the name to assign to the Club
+	 * @return void
+	 */
 	public void setName(String newName) {
 		clubName = newName;
 	}
 	
+	/**
+	 * This function retrieves the list of the athletes in the team
+	 * @return team
+	 */
 	public List<Athlete> getTeam(){
 		return team;
 	}
 	
+	/**
+	 * This function retrieves the list of athletes in the reserves
+	 * @return reserve
+	 */
 	public List<Athlete> getReserve(){
 		return reserve;
 	}
 	
-	public int getNumPosition(int position) {		/*returns the number of players in a specified position
-	 												  1:Forward, 2:Mid, 3:Defense*/
+	/**
+	 * This function returns the number of athletes assigned to the respective position
+	 * on the team
+	 * 
+	 * @param position is the specified position in the format 1:Forward, 2:Mid, 3:Defense
+	 * @return retNum
+	 */
+	public int getNumPosition(int position) {
 		int retNum=0;
 		if (position == 4) {
 			for (Athlete athlete : reserve) {
@@ -46,7 +83,13 @@ public class Club extends GameEnvironment {
 		return retNum;
 	}
 	
-	public void addAthlete(Athlete athlete) {      	/*Adds given athlete to the Club Team*/
+	/**
+	 * This function adds the given athlete to the team
+	 * 
+	 * @param athlete to add to team
+	 * @return void
+	 */
+	public void addAthlete(Athlete athlete) {
 		if (athlete.getPosition()==4) {
 			reserve.add(athlete);
 		} else {
@@ -54,19 +97,38 @@ public class Club extends GameEnvironment {
 		}
 	}
 	
-	public void teamRemoveAthlete(Athlete athlete) {    /*Removes given athlete from the Club Team*/
+	
+	/**
+	 * This function removes the athlete from the team
+	 * 
+	 * @param athlete is the respective athlete to remove
+	 * @return void
+	 */
+	public void teamRemoveAthlete(Athlete athlete) {
 		team.remove(athlete);
 	}
 	
-	public void reserveRemoveAthlete(Athlete athlete) {    /*Removes given athlete from the Club Team*/
+	/**
+	 * This function removes the respective athlete from the reserves
+	 * 
+	 * @param athlete is the respective athlete to remove
+	 * @return void
+	 */
+	public void reserveRemoveAthlete(Athlete athlete) {
 		reserve.remove(athlete);
 	}
 	
+	/**
+	 * This function checks if the team is full or not
+	 * 
+	 * @return teamFull
+	 */
 	public Boolean checkTeamFull() {   /*Checks if the team is full*/
+		Boolean teamFull = false;
 		if (getNumPosition(1)==2 & getNumPosition(2)==3 & getNumPosition(3)==2) {
-			return true;
+			teamFull = true;
 		}
-		return false;
+		return teamFull;
 	}
 	
 	public Boolean ReserveFull(){
